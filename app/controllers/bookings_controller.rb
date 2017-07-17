8c369c46ea3334107852b4ced4d87fd1904459a2
Francois Joseph Bouyer <fjbouyer@gmail.com>
2017-07-17 19:09:55 +0200

    Add Booking

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,30 @@
+class BookingsController < ApplicationController
+  before_action :find_booking, only: [:show]
+  skip_before_action :authenticate_user!, only: :new
+
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status)
+  end
+
+  def find_booking
+    @booking = Booking.find(params[:id])
+  end
+end
