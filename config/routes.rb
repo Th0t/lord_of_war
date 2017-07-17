22c4e16588cde79c3918bcb3880ebe2df90c6b9a
Julien Vanderstock <julien.vanderstock@openmailbox.org>
2017-07-17 17:54:41 +0200

    Correct Routes and seeds

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,27 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :weapons, only: [ :index, :show]
+  resources :bookings, only: [ :show, :new, :create]
+
+  # get "/bookings/:id/reject",  to: "bookings#reject", as: "accept_booking"
+  # get "/bookings/:id/accept",  to: "bookings#accept", as: "reject_booking"
+
+  namespace :account do
+    # get "/",             to: "profile#show"
+    # get "/bookings",     to: "profile#bookings"
+    # get "/reservings",   to: "profile#reservings"
+    # get "/weapons",      to: "profile#weapons"
+
+    resource :profile, only: [:show]
+    resources :bookings_made_by_me, only: [:index]
+    resources :bookings_made_to_me, only: [:index] do
+      member do
+        patch :accept
+        patch :reject
+      end
+    end
+    resources :weapons, only: [:index, :new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times { |i| User.create(first_name:         "test#{i}",
+                          last_name:          "test#{i}",
+                          email:              "test#{i}@test.fr",
+                          password:           "teeest#{i}") }
+
+5.times { |i| Weapon.create(title:            "test#{i}",
+                            description:      "test#{i}",
+                            category:         "knives",
+                            price:            42,
+                            owner_id:         i+1) }
+
+5.times { |i| Booking.create(start_date:      Date.new,
+                             end_date:        Date.new,
+                             status:          "pending",
+                             hirer_id:        i+1,
+                             weapon:          Weapon.find(i+1)) }
+
+5.times { |i| Booking.create(start_date:      Date.new,
+                             end_date:        Date.new,
+                             status:          "pending",
+                             hirer_id:        i+1,
+                             weapon_id:       i+1) }
