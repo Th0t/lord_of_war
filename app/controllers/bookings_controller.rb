d370caa5911ab7b7afe89eaee09ad452fa6c00eb
Francois Joseph Bouyer <fjbouyer@gmail.com>
2017-07-20 14:10:52 +0200

    Add bookings#create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-  before_action :find_booking, only: [:show, :total_price, :number_of_days, :available]
+  before_action :find_booking, only: [:show]
   skip_before_action :authenticate_user!, only: :new
 
   def show
@@ -12,7 +12,7 @@ class BookingsController < ApplicationController
     @end_date = DateTime.strptime(booking_params[:end_date], '%m/%d/%Y').to_date
     @number_of_days = (@end_date - @start_date).to_i
     if @number_of_days <= 0
-      flash[:alert] = "Wrong dates : end date must be superior to start date"
+      flash[:alert] = "Wrong dates: end date must be superior to start date"
       redirect_to weapon_path(@weapon)
     else
       @total_price = @weapon.price * @number_of_days
@@ -20,22 +20,25 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @weapon = Weapon.find(params[:weapon_id])
     @booking = Booking.new(booking_params)
     if @booking.save
-      redirect_to booking_path(@booking)
+      redirect_to account_bookings_made_by_me_path
+      flash[:notice] = "Your booking request has been sent to the owner"
     else
-      render :new
+      redirect_to weapon_path(@weapon)
+      flash[:notice] = "Your booking has failed, please try again"
     end
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :status)
+    params.require(:booking).permit(:start_date, :end_date, :hirer_id, :weapon_id, :status)
   end
 
   def find_booking
     @booking = Booking.find(params[:id])
   end
 
-end
+  end
