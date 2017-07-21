6b540c1d0f085e13cdd1960c287e7c94ac6ee500
Julien Vanderstock <julien.vanderstock@openmailbox.org>
2017-07-21 10:48:55 +0200

    Done edit profile page, still need to implement avatar_url functionnality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  as :user do
+    get 'account/profile/edit', to: 'devise/registrations#edit'
+    patch 'account/profile', :as => :user_root
+  end
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
